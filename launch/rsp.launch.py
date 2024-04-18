68c94a1bf60d108eafd0b6d93834b5504960a2c2
vahnroyjr <vahnroyhosearjr@gmail.com>
2024-04-18 18:09:47 -0500

    updated urdf files

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -19,9 +19,14 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('my_bot'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
+
+    
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    robot_description_xml = robot_description_config.toxml()
+    print(robot_description_xml)
+    
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
